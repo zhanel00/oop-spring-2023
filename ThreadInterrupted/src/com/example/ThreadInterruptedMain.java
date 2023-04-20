9da5d5b76f045d5678bc10bf358d269c278d7998
zhanel00 <zhanelgmstm@gmail.com>
2023-04-20 19:31:50 +0600

    labs

---
+++ ThreadInterrupted/src/com/example/ThreadInterruptedMain.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import javax.imageio.IIOException;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ThreadInterruptedMain {
+    public static void main(String[] args) {
+        Runnable r = new Counter();
+        Thread t = new Thread(r);
+        t.start();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            br.readLine();
+        } catch (IOException e) {}
+
+        System.out.println("Thread is alive: " + t.isAlive());
+        t.interrupt();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {}
+
+        System.out.println("Thread is alive: " + t.isAlive());
+    }
+}
